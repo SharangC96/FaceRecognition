de239511ae3252b703ea82490ef2e73922e120a6
SharangC <sharang.chopra115@gmail.com>
2017-12-29 03:35:19 +0530

    path changes

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from Gallery import Gallery
+import os
 
-gallery_dir = '/home/sharang/Pictures/Dataset'
+dirname = os.path.dirname(__file__)
+gallery_dir = os.path.join(dirname,'Dataset')
+
+#gallery_dir = 'Dataset/'
 mygallery = Gallery(gallery_dir,name='MyGallery',reuse= False)
 
-print(mygallery.recognize('/home/sharang/Pictures/test.jpg'))
+print(mygallery.recognize(os.path.join(dirname,'test.jpg')))
+# print(mygallery.recognize('test.jpg'))

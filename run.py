081912385979e4844106c8bc8b903968a52b619b
SharangC <sharang.chopra115@gmail.com>
2017-12-30 15:19:32 +0530

    use folder for images

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ from Gallery import Gallery
 import os
 
 dirname = os.path.dirname(__file__)
-gallery_dir = os.path.join(dirname,'Dataset')
+gallery_dir = os.path.join(dirname,'lfw')
 
 #gallery_dir = 'Dataset/'
-mygallery = Gallery(gallery_dir,name='MyGallery',reuse= False)
+mygallery = Gallery(gallery_dir,name='lfw',reuse= False)
 
-print(mygallery.recognize(os.path.join(dirname,'test.jpg')))
+#print(mygallery.recognize(os.path.join(dirname,'test.jpg')))
 # print(mygallery.recognize('test.jpg'))
